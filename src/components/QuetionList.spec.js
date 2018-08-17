3c6d5b542cd29525329c3b60896a1f12146f100f
Ovidio Calvet <calveto@gmail.com>
2018-08-16 20:22:56 -0400

    Add async test

--- src/components/QuetionList.spec.js
+++ src/components/QuetionList.spec.js
@@ -1,5 +1,11 @@
+import { delay } from 'redux-saga';
+
 describe(`QuestionList`, () => {
   it(`should display a list of items`, () => {
     expect(2 + 2).toEqual(4);
+  });
+  it(`should resolve after async function`, async () => {
+    await delay(100);
+    expect(3).toBe(3);
   });
 });
